f2b1ad4a0d0d59d70936bfdc04b8a353bf52ec1d
Roeut Chak <roeut.chak@gmail.com>
2018-12-10 23:56:21 -0500

    Initial Commit

---
+++ LostCash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LostCash
+//
+//  Created by Apple on 12/10/18.
+//  Copyright © 2018 Roeut Chak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
